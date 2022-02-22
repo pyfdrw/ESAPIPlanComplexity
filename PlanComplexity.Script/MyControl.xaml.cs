11f796e49bbf98c67c500e6d22aea0376b34f94f
pyfdrw <tobepyf@hotmail.com>
2022-02-22 21:51:47 +0800

    更改程序错误，现在使用主线程进行参数计算

--- PlanComplexity.Script/MyControl.xaml.cs
+++ PlanComplexity.Script/MyControl.xaml.cs
@@ -224,10 +224,148 @@ namespace PlanComplexity.Script
         // 开始运行
         private void DoAnalyse_Button_Click(object sender, RoutedEventArgs e)
         {
-            if (bcWorker != null && !bcWorker.IsBusy)
+            if (app == null)
             {
-                bcWorker.RunWorkerAsync();
+                return;
             }
+            int totalPatient = dataLayer.PatientIdList.Count;
+            if (0 == totalPatient)
+            {
+                theRunMessage.AddMessage("未指定患者ID，查找指定日期内的所有患者信息");
+            }
+            List<string> patientList = app.PatientSummaries.Where(x => x.CreationDateTime >= dataLayer.BeginTime && x.CreationDateTime <= dataLayer.EndTime).Select(x => x.Id).ToList();
+
+            if (patientList.Count > 0)
+            {
+                totalPatient = patientList.Count;
+                theRunMessage.AddMessage($"所有患者数量为 {totalPatient}");
+            }
+
+            theRunMessage.AddMessage($"输出文件夹: {dataLayer.OutputFolder}");
+
+            // 查找到的所有患者
+            foreach (var item in patientList)
+            {
+                dataLayer.PatientIdList.Add(new PatientIdClass() { PatientId = item });
+            }
+
+            float finishedNum = 0;
+            // 对每一个患者进行计算
+
+            String vmat_filename = Path.Combine(dataLayer.OutputFolder, "patient_vmat_analysis.csv");
+            theRunMessage.AddMessage($"输出文件: {vmat_filename}");
+            StreamWriter vmat_patient_file = new StreamWriter(vmat_filename, true, Encoding.GetEncoding("gb2312"));
+
+            vmat_patient_file.WriteLine("ID, Name, Sites, Doctor, Physicist, PlanID, MachineID, Calculation_Model, Prescribed_Dose, MU, " +
+                "EDGE_Metric, Leaf_Area, Plan_Irregularity, Plan_Modulation, Modulation_Complexity_Score, " +
+                "Small_Aperture_Score_5mm, Small_Aperture_Score_10mm, Small_Aperture_Score_20mm, Mean_Field_Area, " +
+                "Mean_Asymmetry_Distance, Aperture_Area_Ration_Jaw_Area, Aperture_Sub_Regions, Aperture_X_Jaw_Distance, " +
+                "Aperture_Y_Jaw_Distance, Leaf_Gap_Average, Leaf_Gap_Std, Leaf_Travel, Converted_Aperture_Metric, Edge_Area_Metric, " +
+                "Speed_0_4, Speed_4_8, Speed_8_12, Speed_12_16, Speed_16_20, Speed_20_25, Speed_Average, Speed_Std, " +
+                "Acc_0_10, Acc_10_20, Acc_20_40, Acc_40_60, Acc_Average, Acc_std, SPORT");
+
+            String imrt_filename = Path.Combine(dataLayer.OutputFolder, "patient_imrt_analysis.csv");
+            theRunMessage.AddMessage($"输出文件: {imrt_filename}");
+            StreamWriter imrt_patient_file = new StreamWriter(imrt_filename, true, Encoding.GetEncoding("gb2312"));
+            imrt_patient_file.WriteLine("ID, Name, Sites, Doctor, Physicist, PlanID, MachineID, Calculation_Model, Prescribed_Dose, MU, " +
+                                        "EDGE_Metric, Leaf_Area, Plan_Irregularity, Plan_Modulation, Modulation_Complexity_Score, " +
+                                        "Small_Aperture_Score_5mm, Small_Aperture_Score_10mm, Small_Aperture_Score_20mm, Mean_Field_Area, " +
+                                        "Mean_Asymmetry_Distance, Aperture_Area_Ration_Jaw_Area, Aperture_Sub_Regions, Aperture_X_Jaw_Distance, " +
+                                        "Aperture_Y_Jaw_Distance, Leaf_Gap_Average, Leaf_Gap_Std, Leaf_Travel, Converted_Aperture_Metric, Edge_Area_Metric");
+
+            foreach (var patientIdInstance in dataLayer.PatientIdList)
+            {
+                try
+                {
+                    Patient patient = app.OpenPatientById(patientIdInstance.PatientId);
+                    if (patient == null)
+                    {
+                        patient = app.OpenPatientById(patientIdInstance.PatientId.ToLower());
+                    }
+
+                    theRunMessage.AddMessage($"处理: {patient.Id}");
+
+                    if (patient != null)
+                    {
+                        Console.WriteLine($"Open Patient by Id = {patient.Id}");
+
+#if (DEBUG136)
+                        // Only choose DMLC/IMRT and VMAT plan to analysis
+                        var approvedPlans = from Course c in patient.Courses
+                                            where c != null
+                                            from PlanSetup ps in c.PlanSetups
+
+                                            where ps.ApprovalStatus == PlanSetupApprovalStatus.TreatmentApproved && ps.IsTreated == true
+                                            select new
+                                            {
+                                                Patient = patient,
+                                                Cource = c,
+                                                Plan = ps
+                                            };
+#endif
+#if (!DEBUG136)
+
+                // Only choose DMLC/IMRT and VMAT plan to analysis
+                        var approvedPlans = from Course c in patient.Courses
+                                            where c != null
+                                            from PlanSetup ps in c.PlanSetups
+
+                                            where ps.ApprovalStatus == PlanSetupApprovalStatus.TreatmentApproved && ps.PlanIntent.ToLower() != "verification"
+                                            select new
+                                            {
+                                                Patient = patient,
+                                                Cource = c,
+                                                Plan = ps
+                                            };
+#endif
+
+
+                        theRunMessage.AddMessage($"该患者下符合条件的计划数量: {approvedPlans.Count()}");
+                        if (approvedPlans.Any())
+                        {
+                            foreach (var p in approvedPlans)
+                            {
+                                PlanSetup ps = p.Plan;
+                                Console.WriteLine($"Open Course by Id = {p.Cource.Id}, Open Plan by ID = {ps.Id}");
+
+                                if (PlanUtilities.getTreatmentTechnique(ps).ToUpper().Equals("STATIC"))
+                                {
+                                    string[] patient_infos = new String[] { patient.Id, patient.Name, "", "", "" };
+                                    IMRT.Execute(p.Patient, ps, imrt_patient_file, patient_infos);
+                                }
+                                else if (PlanUtilities.getTreatmentTechnique(ps).ToUpper().Equals("ARC"))
+                                {
+                                    string[] patient_infos = new String[] { patient.Id, patient.Name, "", "", "" };
+                                    VMAT.Execute(p.Patient, ps, vmat_patient_file, patient_infos);
+                                }
+                                theRunMessage.AddMessage($"{patient.Id} {patient.Name} Course:{ps.Course.Id} Plan:{ps.Id} 计算完成");
+                            }
+                            theRunMessage.AddMessage($"该患者处理完成");
+
+                        }
+                    }
+                    app.ClosePatient();
+
+                    finishedNum++;
+                    // bcWorker.ReportProgress((int)Math.Round(finishedNum / totalPatient * 100));
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine(exception);
+                    theRunMessage.AddMessage(exception.Message);
+
+                    app.ClosePatient();
+                    // throw;
+                }
+            }
+            imrt_patient_file.Close();
+            vmat_patient_file.Close();
+
+
+            //if (bcWorker != null && !bcWorker.IsBusy)
+            //{
+            //    bcWorker.RunWorkerAsync();
+            //}
         }
 
         // 取消
